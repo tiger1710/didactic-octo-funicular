5d21d475885660c80f51169797708cca45250d93
tiger1710 <tiger1710p@gmail.com>
2020-02-03 14:11:23 +0900

    2020. 2. 3

---
+++ source/codes/2644.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <limits>
+#include <string>
+#define endl '\n'
+#define point pair<int, int>
+using namespace std;
+
+point operator+(const point& A, const point& B) {
+    return point(A.first + B.first, A.second + B.second);
+}
+
+int bfs(vector<vector<int>>& people, const int& from, const int& to) {
+    queue<int> q;
+    vector<int> visit(people.size());
+
+    q.push(from);
+    visit[from] = 1;
+
+    while (not q.empty()) {
+        int cur = q.front(); q.pop();
+        if (cur == to) return visit[cur] - 1;
+
+        for (int& next : people[cur]) {
+            if (not visit[next]) {
+                q.push(next);
+                visit[next] = visit[cur] + 1;
+            }
+        }
+    }
+
+    return -1;
+}
+
+int main(void) {
+    ios_base::sync_with_stdio(false);
+
+    int n; cin >> n;
+    int from, to; cin >> from >> to; from--, to--;
+    int m; cin >> m;
+
+    vector<vector<int>> people(n);
+
+    for (int i = 0; i < m; i++) {
+        int x, y; cin >> x >> y;
+        people[x - 1].push_back(y - 1);
+        people[y - 1].push_back(x - 1);
+    }
+
+    cout << bfs(people, from, to) << endl;
+
+    return 0;
+}
