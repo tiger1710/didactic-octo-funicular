dccc9c84bac4effa27633a3670005a48c82279b1
tiger1710 <tiger1710p@gmail.com>
2020-02-18 17:59:38 +0900

    thinking...

--- source/codes/4179.cpp
+++ source/codes/4179.cpp
@@ -13,7 +13,8 @@ private:
     int R, C;
     vector<vector<char>> maze;
     vector<vector<int>> visit;
-    vector<point> jihun_and_fire;
+    vector<point> fire;
+    point J;
     char& pos(const point& p) {
         return maze[p.first][p.second];
     }
@@ -24,16 +25,17 @@ private:
     void input(void) {
         cin >> R >> C; cin.ignore();
         maze = vector<vector<char>>(R, vector<char>(C));
+        visit = vector<vector<int>>(R, vector<int>(C));
         for (auto& row : maze) {
             for (auto& col : row) {
                 cin >> col;
                 switch (col) {
-                    case 'J':
-                    case 'F':
-                    jihun_and_fire.push_back(point(&row - &maze[0], &col - &row[0]));
-                    break;
+                    case 'J': J = point(&row - &maze[0], &col - &row[0]);
+                        break;
+                    case 'F': fire.push_back(point(&row - &maze[0], &col - &row[0]));
+                        break;
                     default:
-                    break;
+                        break;
                 }
             }
         }
