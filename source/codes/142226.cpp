65148655115e508cf76fe69b9c832219fe6f1c61
tiger1710 <tiger1710p@gmail.com>
2020-02-28 17:49:13 +0900

    2020. 2. 28.

---
+++ source/codes/142226.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <algorithm>
+#include <functional>
+#include <vector>
+#include <queue>
+#include <limits>
+#include <string>
+#include <unordered_map>
+#define endl '\n'
+using namespace std;
+typedef pair<int, int> point; //cnt, clipboard
+
+class Emoji {
+private:
+    int S, ans;
+    unordered_map<point, int> visit;
+
+    point copy(const point& p) const { return point(p.first, p.first); };
+    point paste(const point& p) const { return point(p.first + p.second, p.second); }
+    point del(const point& p) const { return point(p.first - 1, p.second); }
+    function<point(const Emoji&, const point& p)> fp[3] = { copy, paste, del };
+
+    bool visited(const point& p) { return visit.find(p) not_eq visit.end(); }
+
+    void input(void) { 
+        cin >> S;
+    }
+
+    void calc(void) {
+        queue<point> q;
+        q.push(point(1, 0));
+        visit[point(1, 0)] = 1;
+
+        while (not q.empty()) {
+            point cur = q.front(); q.pop();
+            if (cur.first == S) {
+                ans = visited(cur) - 1;
+                break;
+            }
+            for (auto& call : fp) {
+                point next = call(*this, cur);
+                if (next.first >= 0 and
+                    not visited(next)) {
+                    q.push(next); visit[next] = visit[cur] + 1;
+                }
+            }
+        }
+    }
+    void output(void) {
+        cout << ans;
+    }
+public:
+    void solve(void) {
+        input();
+        calc();
+        output();
+    }
+};
+
+int main(void) {
+    ios_base::sync_with_stdio(false);
+
+    Emoji e;
+    e.solve();
+
+    return 0;
+}
--- source/codes/2251.cpp
+++ source/codes/2251.cpp
@@ -7,21 +7,12 @@
 #define endl '\n'
 using namespace std;
 typedef pair<int, int> point;
+class Calc;
 
 class Bottle {
 private:
     int volume[3];
     int buket[3] = { 0, 0, 0 };
-
-    void pour(int& from, int& to) {
-        if (volume[to] - buket[to] >= buket[from]) {
-            buket[to] += buket[from];
-        }
-        else {
-            buket[from] -= (volume[to] - buket[to]);
-            buket[to] = volume[to];
-        }
-    }
 
     void input(void) {
         for (int& i : volume) {
@@ -31,14 +22,84 @@ private:
         buket[2] = volume[2];
     }
 public:
+    friend Calc;
+    int Abuket(void) {
+        return buket[0];
+    }
+    int Cbuket(void) {
+        return buket[2];
+    }
+
+    void pour(const int& from, const int& to) {
+        if (volume[to] - buket[to] >= buket[from]) {
+            buket[to] += buket[from];
+            buket[from] = 0;
+        }
+        else {
+            buket[from] -= (volume[to] - buket[to]);
+            buket[to] = volume[to];
+        }
+    }
+
+    Bottle(void) {
+        input();
+    }
 };
 
-class {
+class Calc{
+private:
+    Bottle start;
+    vector<vector<vector<bool>>> visit;
+    vector<int> ans;
 
+    vector<bool>::reference visited(const Bottle& b) {
+        return visit[b.buket[0]][b.buket[1]][b.buket[2]];
+    }
+    
+
+    void init(void) {
+        visit = vector<vector<vector<bool>>>(201, vector<vector<bool>>(201, vector<bool>(201)));
+    }
+    void calc(void) {
+        queue<Bottle> q;
+        q.push(start); visited(start) = true;
+        ans.push_back(start.Cbuket());
+
+        while (not q.empty()) {
+            Bottle cur = q.front(); q.pop();
+            for (int from = 0; from < 3; from++) {
+                for (int to = 0; to < 3; to++) {
+                    if (from == to) continue;
+                    Bottle next = cur; next.pour(from, to);
+                    if (not visited(next)) {
+                        visited(next) = true;
+                        q.push(next);
+                        if (next.Abuket() == 0) ans.push_back(next.Cbuket());
+                    }
+                }
+            }
+        }
+    }
+    void output(void) {
+        sort(ans.begin(), ans.end());
+        ans.erase(unique(ans.begin(), ans.end()), ans.end());
+        for (int& i : ans) {
+            cout << i << ' ';
+        }
+    }
+public:
+    void solve(void) {
+        init();
+        calc();
+        output();
+    }
 };
 
 int main(void) {
     ios_base::sync_with_stdio(false);
+
+    Calc calc;
+    calc.solve();
 
     return 0;
 }
