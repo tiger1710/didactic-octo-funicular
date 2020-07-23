0c1e140a38f69236a101db1e65f3e66075ce81d9
Deok Hwan Kim <tiger1710p@gmail.com>
2020-07-23 17:14:51 +0900

    2020. 7. 23. (OCR이란?... 대박...)

--- source/codes/algospot.com/OCR.cpp
+++ source/codes/algospot.com/OCR.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <cmath>
 #define endl '\n'
 using namespace std;
 
@@ -9,9 +10,11 @@ private:
     int m, q, n;//m:원문 단어수, q:query, n 변환문장단어수
     vector<string> corpus;//단어들
     vector<vector<double>> T, M, cache;//T:i단어 다음이 k단어일 확률. / M:i단어를 k단어로 분류할 확률.
-    vector<double> B;//첫단어로 등장할 확률.
+    vector<double> B;
     vector<int> R;//분류기가 반환한 문장. 번호로 변환.
     vector<vector<int>> choice;
+
+    string ans;
 
 
     double recognize(const int& segment, const int& previousMatch) {
@@ -21,8 +24,8 @@ private:
         ret = -1e200;//log(0) == -INF
         int& choose = choice[segment][previousMatch];
 
-        for (int thisMatch = 0; thisMatch < m; thisMatch++) {
-            //logA + logB = logAB (확률의 곱)
+        for (int thisMatch = 1; thisMatch <= m; thisMatch++) {
+            //logA + logB = logAB (확률의 곱), previousMatch = 0 이면 첫등장 확률(B).
             double cand = T[previousMatch][thisMatch]//previousMatch단어 다음이 thisMatch단어일 확률
                             + M[thisMatch][R[segment]]//thisMatch단어를 R[segment]단어로 분류할 확률
                             + recognize(segment + 1, thisMatch);
@@ -36,17 +39,21 @@ private:
     }
     string reconstruct(const int& segment, const int& previousMatch) {
         int choose = choice[segment][previousMatch];
-        string ret = corpus[choose];
+        string ret = corpus[choose - 1];
         if (segment < n - 1)
             ret = ret + " " + reconstruct(segment + 1, choose);
         return ret;
     }
     void init() {
         cin >> n;
+        R = vector<int>(n);
         for (int words = 0; words < n; words++) {
             string str; cin >> str;
-            R[words] = find(corpus.begin(), corpus.end(), str) - corpus.begin();
+            R[words] = find(corpus.begin(), corpus.end(), str) - corpus.begin() + 1;
         }
+        //log1 = 0
+        cache = vector<vector<double>>(n + 1, vector<double>(1 + m, 1.0));
+        choice = vector<vector<int>>(n + 1, vector<int>(1 + m));
     }
 
     void input(void) {
@@ -56,29 +63,49 @@ private:
         for (string& i : corpus) cin >> i;
         
         B = vector<double>(m);
-        for (double& i : B) cin >> i;
+        T = vector<vector<double>>(m + 1, vector<double>(m + 1));
+        M = vector<vector<double>>(m + 1, vector<double>(m + 1));
 
-        T = M = vector<vector<double>>(m, vector<double>(m));
-        //log1 = 0
-        cache = vector<vector<double>>(m, vector<double>(m, 1.0));
+        for (int c = 1; c <= m; c++) {
+            cin >> T[0][c];
+            T[0][c] = log(T[0][c]);
+        }
 
-        for (vector<double>& r : T)
-            for (double& c : r)
-                cin >> c;
-        for (vector<double>& r : M)
-            for (double& c : r)
-                cin >> c;
+        for (int r = 1; r <= m; r++)
+            for(int c = 1; c <= m; c++) {
+                cin >> T[r][c];
+                T[r][c] = log(T[r][c]);
+            }
+
+        for (int r = 1; r <= m; r++)
+            for(int c = 1; c <= m; c++) {
+                cin >> M[r][c];
+                M[r][c] = log(M[r][c]);
+            }
     }
     void calc(void) {
-        init();
-        double val = recognize(0, 0);
+        while (q--) {
+            init();
+            double val = recognize(0, 0);
+            ans = reconstruct(0, 0);
+            output();
+        }
+    }
+    void output(void) {
+        cout << ans << endl;
     }
 public:
+    void solve(void) {
+        input();
+        calc();
+    }
 };
 
 int main(void) {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
+
+    OCR().solve();
 
     return 0;
 }
