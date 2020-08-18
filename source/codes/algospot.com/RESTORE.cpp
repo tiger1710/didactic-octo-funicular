31dd0b60a2ec21c74764a81928664ef640680056
Deok Hwan Kim <tiger1710p@gmail.com>
2020-08-18 15:35:01 +0900

    2020. 8. 18. (RESTORE)

--- source/codes/algospot.com/RESTORE.cpp
+++ source/codes/algospot.com/RESTORE.cpp
@@ -8,8 +8,20 @@ using namespace std;
 class Restore {
 private:
     int k;
+    string ans;
     vector<string> word;
     vector<vector<int>> cache, overlap;
+
+    void init(void) {
+        for (int last = 0; last < word.size(); last++)
+            for (int next = 0; next < word.size(); next++)
+                if (last not_eq next and
+                    word[last].find(word[next]) not_eq string::npos) {
+                        word.erase(word.begin() + next);
+                        last--;
+                        break;
+                    }
+    }
 
     int restore(const int& last, const int& used) {
         if (used == (1 << k) - 1) return 0;
@@ -27,11 +39,13 @@ private:
         return ret;
     }
     int d(const string& a, const string& b) {
-        int maxOverlap = 0;
-        for (int i = 0; i < b.length(); i++)
-            if (a.substr(a.length() - 1 - i) == b.substr(0, i + 1))
-                maxOverlap++;
-            else return maxOverlap;
+        int maxOverlap = 0, l = a.length() < b.length() ? a.length() : b.length();
+        for (int i = 0; i < l; i++) {
+            string Asubstr = a.substr(a.length() - 1 - i);
+            string Bsubstr = b.substr(0, i + 1);
+            if (Asubstr == Bsubstr)
+                maxOverlap = Asubstr.length();
+        }
         return maxOverlap;
     }
 
@@ -52,19 +66,26 @@ private:
         cin >> k;
         word = vector<string>(k);
         for (string& str : word) cin >> str;
-        cache = vector<vector<int>>(k, vector<int>(1 << k, -1));
+        init();
+        cache = vector<vector<int>>(k + 1, vector<int>(1 << k, -1));
         overlap = vector<vector<int>>(k, vector<int>(k));
+        overlap.push_back(vector<int>(k, 0));
 
         //overlap[a][b] = a의 접미사, b의 접두사 겹치는 크기
         for (int last = 0; last < k; last++)
-            for (int next = 0; next < k; next++)
+            for (int next = 0; next < k; next++) {
+                if (last == next) {
+                    overlap[last][next] = 0;
+                    continue;
+                }
                 overlap[last][next] = d(word[last], word[next]);
+            }
     }
     void calc(void) {
-        int cnt = restore(0, 0);
+        ans = reconstruct(k, 0);
     }
     void output(void) {
-
+        cout << ans << endl;
     }
 public:
     void solve(void) {
